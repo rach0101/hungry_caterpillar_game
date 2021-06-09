45077b18564ca579de6dbeffb1acc0e5104b1716
Rachel Cleak <rachelmcleak@gmail.com>
2021-06-08 18:58:32 -0700

    Final update to Hungry Caterpillar Game

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const grid = document.querySelector('.grid')
 const startButton = document.getElementById('start')
 const scoreDisplay = document.getElementById('score')
 let squares = []
-let currentSnake = [2,1,0]
+let currentCaterpillar = [2,1,0]
 let direction = 1
 const width = 10
 let appleIndex = 0
 let score = 0
 let intervalTime = 1000
-let speed = 0.9
+let speed = .9
 let timerId = 0
 
 function createGrid() {
@@ -26,15 +26,15 @@ function createGrid() {
 }
 createGrid()
 
-currentSnake.forEach(index => squares[index].classList.add('snake'))
+currentCaterpillar.forEach(index => squares[index].classList.add('caterpillar'))
 
 function startGame() {
-    //remove the snake
-    currentSnake.forEach(index => squares[index].classList.remove('snake'))
+    //remove the caterpillar
+    currentCaterpillar.forEach(index => squares[index].classList.remove('caterpillar'))
     //remove the apple
     squares[appleIndex].classList.remove('apple')
     clearInterval(timerId)
-    currentSnake = [2,1,0]
+    currentCaterpillar = [2,1,0]
     score = 0
     //re add new score to browser
     scoreDisplay.textContent = score
@@ -42,58 +42,63 @@ function startGame() {
     intervalTime = 1000
     generateApple()
     //readd the class of snake to our new currentSnake
-    currentSnake.forEach(index => squares[index].classList.add('snake'))
+    currentCaterpillar.forEach(index => squares[index].classList.add('caterpillar'))
     timerId = setInterval(move, intervalTime)
 }
 
 function move() {
     if (
-        (currentSnake[0] + width >= width*width && direction === width) || //if snake has hit bottom
-        (currentSnake[0] % width === width-1 && direction === 1) || //if snake has hit right wall
-        (currentSnake[0] % width === 0 && direction === -1) || //if snake has hit left wall
-        (currentSnake[0] - width < 0 && direction === -width) || //if snake has hit top
-        squares[currentSnake[0] + direction].classList.contains('snake')
-    )
-    return clearInterval(timerId)
+        (currentCaterpillar[0] + width >= width*width && direction === width) || //if caterpillar has hit bottom
+        (currentCaterpillar[0] % width === width-1 && direction === 1) || //if caterpillar has hit right wall
+        (currentCaterpillar[0] % width === 0 && direction === -1) || //if caterpillar has hit left wall
+        (currentCaterpillar[0] - width < 0 && direction === -width) || //if caterpillar has hit top
+        squares[currentCaterpillar[0] + direction].classList.contains('caterpillar') //if caterpillar hits itself
+        
+    ){
+        console.log("game over");
+        document.getElementById("overlay").style.display = "block";
+        return clearInterval(timerId)
+    }
+    
 
-    //remove last element from our currentSnake array
-    const tail = currentSnake.pop()
+    //remove last element from our currentCaterpillar array
+    const tail = currentCaterpillar.pop()
     //remove styling from last element
-    squares[tail].classList.remove('snake')
+    squares[tail].classList.remove('caterpillar')
     //add square in direction we are heading
-    currentSnake.unshift(currentSnake[0] + direction)
+    currentCaterpillar.unshift(currentCaterpillar[0] + direction)
     //add styling so we can see it
     
-    //deal with snake head gets apple
-    if (squares[currentSnake[0]].classList.contains('apple')) {
+    //deal with caterpillar head gets apple
+    if (squares[currentCaterpillar[0]].classList.contains('apple')) {
         //remove the class of apple
-        squares[currentSnake[0]].classList.remove('apple')
+        squares[currentCaterpillar[0]].classList.remove('apple')
         //grow our snake by adding class of snake to it
-        squares[tail].classList.add('snake')
+        squares[tail].classList.add('caterpillar')
         console.log(tail)
         //grow our snake array
-        currentSnake.push(tail)
-        console.log(currentSnake)
+        currentCaterpillar.push(tail)
+        console.log(currentCaterpillar)
         //generate new apple
         generateApple()
         //add one to the score
         score++
         //display our score
         scoreDisplay.textContent = score
-        //speed up our snake
+        //speed up our caterpillar
         clearInterval(timerId)
         console.log(intervalTime)
         intervalTime = intervalTime * speed
         console.log(intervalTime)
         timerId = setInterval(move, intervalTime)
     } 
-    squares[currentSnake[0]].classList.add('snake')
+    squares[currentCaterpillar[0]].classList.add('caterpillar')
 }
 
 function generateApple() {
     do {
         appleIndex = Math.floor(Math.random() * squares.length)
-    } while (squares[appleIndex].classList.contains('snake'))
+    } while (squares[appleIndex].classList.contains('caterpillar'))
     squares[appleIndex].classList.add('apple')
 } 
 generateApple()
